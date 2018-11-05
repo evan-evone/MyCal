fecef65444784b2b4dca674aa482cd4541114aa5
Evan Keeton <keetonnj@edison.k12.nj.us>
2018-11-04 20:17:00 -0500

    Fix error: "mycal" would not change starting day for "month"/year"
    - Result of "$other_args" placement AFTER "date" command - now works

--- mycal.sh
+++ mycal.sh
@@ -12,9 +12,9 @@ color=$($wd/color.py '$@')
 other_args=$($wd/arg.py "$@")
 
 if [[ " ${args[@]} " =~ " month " ]]; then
-  gcal -s 1 -H $color -q US_AK $(date "+%m %Y") "$other_args"
+  gcal -s 1 -H $color -q US_AK "$other_args" $(date "+%m %Y")
 elif [[ " ${args[@]} " =~ " year " ]]; then
-  gcal -s 1 -H $color -q US_AK $(date "+%Y") "$other_args"
+  gcal -s 1 -H $color -q US_AK "$other_args" $(date "+%Y")
 else
   gcal -s 1 -H $color -q US_AK "$other_args"
 fi
